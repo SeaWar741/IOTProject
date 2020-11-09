50bdf8334ad37494324d8f620b88b4b6440ed35e
SeaWar741 <juanca741@gmail.com>
2020-11-09 02:42:33 -0600

    added functional marker, to implement loop system with firebase

--- da-weather/src/Components/Home/RightLayout.js
+++ da-weather/src/Components/Home/RightLayout.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {Container,Row,Col } from 'react-bootstrap'
+import {Container,Row,Col,Image } from 'react-bootstrap'
 
-import MapGL, { GeolocateControl,NavigationControl  } from '@urbica/react-map-gl';
+import MapGL, { GeolocateControl,NavigationControl,Marker  } from '@urbica/react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,6 +13,9 @@ const useStyles = makeStyles((theme) => ({
     width:"100%",
     minHeight:"100vh",
     color:"white"
+  },
+  imageMarker:{
+      height:"16vh"
   }
 }));
 
@@ -27,6 +30,29 @@ const RightLayout = ({ classes }) => {
     zoom: 11
   });
 
+  const [position, setPosition] = useState({
+    longitude: -100.286697388,
+    latitude: 25.63986969
+  });
+
+  const style = {
+    padding: '10px',
+    color: '#fff',
+    cursor: 'pointer',
+    background: '#1978c8',
+    borderRadius: '6px'
+  };
+  
+  const onMapClick = (event) => {
+    setPosition({ longitude: event.lngLat.lng, latitude: event.lngLat.lat });
+  };
+  
+  const onMarkerClick = (event) => {
+    alert('You clicked on a station');
+    event.stopPropagation();
+  };
+  
+
   return (
     <div className={classes.Main}>
         <MapGL
@@ -40,6 +66,13 @@ const RightLayout = ({ classes }) => {
         >
             <GeolocateControl position='top-right' />
             <NavigationControl showCompass showZoom position='top-right'/>
+            <Marker
+                longitude={position.longitude}
+                latitude={position.latitude}
+                onClick={onMarkerClick}
+            >
+                <Image src="./img/mapPointer.png" fluid className={classes.imageMarker}/>
+            </Marker>
         </MapGL>
         
     </div>
