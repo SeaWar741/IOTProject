305786ce0598e3b8558baaf39065bd7f2a15764a
SeaWar741 <juanca741@gmail.com>
2020-11-12 20:17:58 -0600

    added changes to car node

--- Nodos/Estacion4/arduinoSender/arduinoSender.ino
+++ Nodos/Estacion4/arduinoSender/arduinoSender.ino
@@ -190,16 +190,16 @@ void loop() {
   //SENSOR ESPECIAL
   //Poner aqui el nombre de la variable y la lectura
   //Ultrasonico
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(trigSensor, LOW); 
   delayMicroseconds(2);
   
   // Sets the trigPin on HIGH state for 10 micro seconds 
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigSensor, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigSensor, LOW);
   
   // Reads the echoPin, returns the sound wave travel time in microseconds 
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoSensor, HIGH);
   
   // Calculating the distance 
   distance= duration*0.034/2;
